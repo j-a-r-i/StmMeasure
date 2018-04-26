5e8cde78426083cd75feff76148286aec057957c
Jari Ojanen <jojanen@elisanet.fi>
2018-04-26 21:46:33 +0300

    test: uses event system and two new test for uart_print

--- test.c
+++ test.c
@@ -1,59 +1,71 @@
 #include "hw.h"
 #include "test.h"
+#include "buffer.h"
+#include "event.h"
 
-//------------------------------------------------------------------------------
-uint8_t isTestEnd()
-{
-    uint8_t retval = 0;
-    
-    if (gEvents & EV_UART1_RX) {
-	if (gUart2Rx == 'q')
-	    retval = 1;
-	
-	gEvents &= ~(EV_UART1_RX);
-    }
-    return retval;
-}
+buffer_t tstBuffer;
+static uint8_t    gRunning;
+static func_event gOrgEvUart;
 
 //------------------------------------------------------------------------------
 void longDelay(uint16_t time)
 {
+#ifdef SIMULATED
+    delay_us(1000);
+#else
     uint16_t i;
     for (i=0; i<time; i++) {
 	delay_us(1000);
     }
+#endif
 }  
 
+//------------------------------------------------------------------------------
+void on_tst_uart_rx(uint8_t ch)
+{
+    if (ch == 'q') {
+	gRunning = 0;
+    }
+}
+
+/*
 //------------------------------------------------------------------------------
 void testRfm12_1()
 {
     uint8_t buffer[] = { 'h', 'e', 'l', 'l', 'o' };
 
-    while (1) {	
+    gRunning = 1;
+    
+    while (gRunning) {	
 	rfm12b_send(&rfm1, buffer, sizeof buffer);
 	longDelay(1000);
 
-	if (isTestEnd())
-	    break;
+	event_handle();
     }
 }
     
 //------------------------------------------------------------------------------
 void testRfm12_2()
 {
-    while (1) {	
+    gRunning = 1;
+    
+    while (gRunning) {	
 	rfm12b_test(&rfm1);
 	longDelay(1000);
 
-	if (isTestEnd())
-	    break;
+	event_handle();
     }
 }
+*/
 
 //------------------------------------------------------------------------------
 void testLed()
 {
-    while (1) {	
+    gRunning = 1;
+    gOrgEvUart = gEvents2Table[EV_UART1_RX].func;
+    gEvents2Table[EV_UART1_RX].func = on_tst_uart_rx;
+    
+    while (gRunning) {	
 	io_set(PIN_LED1);
 	io_set(PIN_LED2);
 	longDelay(1000);
@@ -62,8 +74,45 @@ void testLed()
 	io_clear(PIN_LED2);
 	longDelay(1000);
 
-	if (isTestEnd())
-	    break;
+	event_handle();
     }
+
+    gEvents2Table[EV_UART1_RX].func = gOrgEvUart;
 }
 
+
+//------------------------------------------------------------------------------
+void testPrn1()
+{
+    static uint8_t counter = 0;
+    
+    buffer_clear(&tstBuffer);
+    buffer_str(  &tstBuffer, "test ");
+    buffer_hex8( &tstBuffer, counter);
+    buffer_nl(   &tstBuffer);
+
+    uart_print(UART, &tstBuffer);
+    
+    counter++;
+}
+
+
+//------------------------------------------------------------------------------
+void testPrn2()
+{
+    static uint8_t counter = 0;
+    
+    buffer_clear(&tstBuffer);
+    buffer_str(  &tstBuffer, "test ");
+    buffer_hex8( &tstBuffer, counter);
+    buffer_nl(   &tstBuffer);
+    uart_print(UART, &tstBuffer);
+
+    buffer_clear(&tstBuffer);
+    buffer_str(  &tstBuffer, "second ");
+    buffer_hex8( &tstBuffer, counter);
+    buffer_nl(   &tstBuffer);
+    uart_print(UART, &tstBuffer);
+
+    counter++;
+}
